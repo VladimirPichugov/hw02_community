fec2308b1f3bc9f00ad6e4e301d5830ed8ca0467
Vladimir Pichugov <vd.pichugov@yandex.ru>
2022-09-11 22:52:55 +0300

    Первая итерация

---
+++ yatube/posts/admin.py
@@ -0,0 +1,18 @@
+from .models import Post, Group
+from django.contrib import admin
+# Register your models here.
+
+class PostAdmin (admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    list_editable = ('group',)
+    empty_value_display = '-пусто-'
+
+admin.site.register(Post, PostAdmin) 
+
+class GroupAdmin (admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description',)
+    empty_value_display = 'пусто'
+
+admin.site.register(Group, GroupAdmin) 
---
+++ yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+# Create your views here.
+def index(request):    
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {'posts': posts,
+               'title': title
+               }
+    return render(request, template, context)
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
+    title = f'Записи сообщества "{group}"'
+    context = {'group': group,
+               'posts': posts,
+               'title': title
+               }
+    return render(request, template, context)
